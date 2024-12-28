ec60ddf70861eac36718cc33a135c12c72e86bc5
Kyle Butler <theokyle@gmail.com>
2024-12-28 17:55:18 -0600

    Added check win function and creation of board in the DOM

--- script.js
+++ script.js
@@ -1,74 +1,101 @@
-function gameBoard() {
-const rows = 3;
-const columns = 3;
-let board = [];
+const gameBoard = (function gameBoard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
 
-const setBoard = () => {
-board = [];
+    const setBoard = () => {
+        for(let y=0; y<rows; y++) {
+        board[y] = [];
+        for(let x=0; x<columns; x++) {
+            board[y].push(cell());
+        }
+    }};
 
-for(let y=0; y<columns; y++) {
-    for(let x=0; x<rows; x++) {
-        board.push(cell());
-    }
-};
-}
+    const getBoard = () => {
+        return board;
+    };
 
-const markSquare = (player, index) => {
-
-};
-
-const getBoard = () => {
-    return board;
-};
-
-const printBoard = () => {
-
-};
-
-const checkWin = () => {
-
-};
-
-return {setBoard, markSquare, getBoard, printBoard, checkWin};
-}
+    setBoard();
+    return {setBoard, getBoard};
+})();
 
 function cell() {
-let value = "";
+    let value = "";
 
-const addToken = (token) => {
-velue = token;
+    const addToken = (token) => {
+    value = token;
+    };
+
+    const getValue = () => value;
+
+    return {addToken, getValue};
 };
 
-const getValue = () => value;
+const game = (function playGame() {
 
-return {addToken, getValue};
-}
+    let round = 0;
+    const board = gameBoard.getBoard();
 
-function playGame() {
+    const players = [
+    {
+        name: "playerOne",
+        token: "X",
+    },
+    {
+        name: "playerTwo",
+        token: "O",
+    }
+    ];
 
-let round = 0;
-const players = [
-{
-    name: playerOne,
-    token: "X",
-},
-{
-    name: playerTwo,
-    token: "O",
-}
-];
+    let activePlayer = players[0];
 
-let activePlayer = players[0];
+    const switchPlayer = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
 
-const switchPlayer = () => {
+    const getActivePlayer = () => activePlayer;
 
-};
+    const printBoard = () => {
+        const boardContainer = document.getElementById("boardContainer");
+        boardContainer.textContent = "";
+        board.forEach((row) => {
+            row.forEach((cell) => {
+                const cellButton = document.createElement("button");
+                cellButton.classList.add("cell");
+                cellButton.textContent = cell.getValue();
+                cellButton.onclick = function() {
+                    cell.addToken(activePlayer.token);
+                    checkWin();
+                    printBoard();
+                }
+                boardContainer.appendChild(cellButton);
+            })
+        })
+    }
 
-const getActivePlayer = () => activePlayer;
+    const checkWin = () => {
+        for(let x = 0; x < 3; x ++) {
+            if(board[x][0].getValue() === activePlayer.token && board[x][1].getValue() === activePlayer.token && board[x][2].getValue() === activePlayer.token){
+                console.log("Win!");
+            }
+        };
 
-const newRound = () => {
+        for(let y = 0; y < 3; y ++) {
+            if(board[0][y].getValue() === activePlayer.token && board[1][y].getValue() === activePlayer.token && board[2][y].getValue() === activePlayer.token){
+                console.log("Win!");
+            }
+        }
 
-};
+        if(board[0][0].getValue() === activePlayer.token && board[1][1].getValue() === activePlayer.token && board[2][2].getValue() === activePlayer.token){
+            console.log("Win!");
+        }
 
-return {switchPlaer, getActivePlayer, newRound};
-}
+        if(board[0][2].getValue() === activePlayer.token && board[1][1].getValue() === activePlayer.token && board[2][0].getValue() === activePlayer.token){
+            console.log("Win!");
+        }
+    }
+
+    return {switchPlayer, getActivePlayer, printBoard};
+})();
+
+game.printBoard();
